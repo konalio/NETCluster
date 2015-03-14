ebd3883a84feadba47f411728280fdd9ae1604a0
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-14 21:35:59 +0100

    Parsing argument from config and arguments.

---
+++ src/CommunicationServer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunicationServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/CommunicationServer/ServerConfig.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+
+namespace CommunicationServer
+{
+    public class ServerConfig
+    {
+        public string ServerPort { get; set; }
+        public bool IsBackup { get; set; }
+        public int ComponentTimeout { get; set; }
+
+        public static ServerConfig LoadFromAppConfig()
+        {
+            var serverPort = ConfigurationManager.AppSettings["ServerPort"];
+            var isBackup = bool.Parse(ConfigurationManager.AppSettings["IsBackup"]);
+            var componentTimeout = int.Parse(ConfigurationManager.AppSettings["ComponentTimeout"]);
+
+            return new ServerConfig
+            {
+                ServerPort = serverPort,
+                IsBackup = isBackup,
+                ComponentTimeout = componentTimeout
+            };
+        }
+
+        public static ServerConfig LoadFromArguments(string[] arguments)
+        {
+            var config = new ServerConfig();
+
+            for (var i = 0; i < arguments.Length; i++)
+            {
+                switch (arguments[i])
+                {
+                    case "-port":
+                    {
+                        try
+                        {
+                            config.ServerPort = arguments[++i];
+                        }
+                        catch (Exception e)
+                        {
+                            throw;
+                        }
+                        break;
+                    }
+                    case "-backup":
+                    {
+                        config.IsBackup = true;
+                        break;
+                    }
+                    case "-t":
+                    {
+                        try
+                        {
+                            var value = arguments[++i];
+                            config.ComponentTimeout = int.Parse(value);
+                        }
+                        catch (Exception e)
+                        {
+                            throw;
+                        }
+                        break;
+                    }
+                }
+            }
+            return config;
+        }
+
+    }
+}
