19a08615094a9b802fc3383c77a3a0b7179c65af
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-15 16:08:18 +0100

    Added tests for basic cases

---
+++ src/test/ClusterUtilsTests/ComponentConfigParserTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using ClusterUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClusterUtilsTests
+{
+    [TestClass]
+    public class ComponentConfigParserTest
+    {
+        [TestMethod]
+        public void CheckConfigAvailability()
+        {
+            var value = ConfigurationManager.AppSettings["ServerAddress"];
+            Assert.IsFalse(String.IsNullOrEmpty(value), "No app config found.");
+        }
+
+        [TestMethod]
+        public void OverrideServerAddressInArgs()
+        {
+            var args = new[] {"-address", "localhost"};
+            var config = ComponentConfig.GetComponentConfig(args);
+
+            Assert.AreEqual(config.ServerAddress, "localhost");
+            Assert.AreEqual(config.ServerPort, "8080");
+        }
+
+        [TestMethod]
+        public void OverrideServerPortInArgs()
+        {
+            var args = new[] {"-port", "7777"};
+            var config = ComponentConfig.GetComponentConfig(args);
+
+            Assert.AreEqual(config.ServerAddress, "127.0.0.1");
+            Assert.AreEqual(config.ServerPort, "7777");
+        }
+    }
+}
