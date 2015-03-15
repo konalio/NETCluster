adbd8fcb1b7fb62e7b92b9b68c7b7e82d07c8421
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-15 12:59:41 +0100

    Server work stub

---
+++ src/CommunicationServer/CommunicationsServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CommunicationServer
+{
+    class CommunicationsServer
+    {
+        private string listeningPort;
+        private bool backupMode;
+        private int componentTimeout;
+        
+        public CommunicationsServer(ServerConfig configuration)
+        {
+            listeningPort = configuration.ServerPort;
+            backupMode = configuration.IsBackup;
+            componentTimeout = configuration.ComponentTimeout;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Server is running...");
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
