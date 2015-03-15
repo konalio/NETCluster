78cb481306e97d2a99ca4186c22422aac8257c71
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-15 15:37:30 +0100

    Added lib to node solution

--- src/ComputationalNode/Program.cs
+++ src/ComputationalNode/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ComputationalNode
+﻿using ClusterUtils;
+
+namespace ComputationalNode
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var config = ComponentConfig.GetComponentConfig(args);
+
         }
     }
 }
