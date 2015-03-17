5070f442023b55b8ef552ac84abf9195a256c8bb
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-18 00:04:24 +0100

    Temporary version of server side register

---
+++ src/ClusterUtils/StateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace ClusterUtils
+{
+    public class StateObject
+    {
+        public Socket WorkSocket = null;
+
+        public const int BufferSize = 1024;
+
+        public byte[] Buffer = new byte[BufferSize];
+
+        public List<byte> ByteBuffer = new List<byte>();
+    }
+}
