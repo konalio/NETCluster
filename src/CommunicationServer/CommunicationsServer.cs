4def4fc0d216033924f6fd6a362e0dc9a1281a11
kocu <kocu@student.mini.pw.edu.pl>
2015-03-17 21:07:43 +0100

    Accidentally deleted to many files, fixed

---
+++ src/CommunicationServer/CommunicationsServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CommunicationServer
+{
+    class CommunicationsServer
+    {
+        private readonly string listeningPort;
+        private bool backupMode;
+        private readonly int componentTimeout;
+        
+        public CommunicationsServer(ServerConfig configuration)
+        {
+            listeningPort = configuration.ServerPort;
+            backupMode = configuration.IsBackup;
+            componentTimeout = configuration.ComponentTimeout;
+        }
+
+        private void LogServerInfo()
+        {
+            Console.WriteLine("Server is running in {0} mode.", backupMode ? "backup" : "primary");
+            Console.WriteLine("Listening on port " + listeningPort);
+            Console.WriteLine("Componenet timeout = {0} [s]", componentTimeout);
+        }
+
+        public void Start()
+        {
+            LogServerInfo();
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
