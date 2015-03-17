509927ae9d20db1785ce7fa1af84c5d315e742f6
kocu <kocu@student.mini.pw.edu.pl>
2015-03-17 15:53:05 +0100

    TestManager, dalsza implementacja

--- src/TaskManager/TaskManager.cs
+++ src/TaskManager/TaskManager.cs
@@ -4,25 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UCCTaskSolver;
-using MessageHandler;
+using ClusterUtils;
 
 namespace TaskManager
 {
-    class TaskManager
+    public class TaskManager
     {
-        List<TaskSolver> ts_List;
+        //List<TaskSolver> ts_List;
         int maxParallelThreads;
-        int port;
 
-        System.Net.IPAddress primaryServer;
-        List<System.Net.IPAddress> backupServers;
+        public string PrimaryServerAddress { get; set; }
+        public string ServerPort { get; set; }
+
+        //int port;
+        //System.Net.IPAddress primaryServer;
+        //List<System.Net.IPAddress> backupServers;
        
-        public TaskManager(List<TaskSolver> ts_, int mPT_, System.Net.IPAddress primary_, int port_)
+        public TaskManager(int mPT_)
         {
-            ts_List = new List<TaskSolver>(ts_);
+            //ts_List = new List<TaskSolver>(ts_);
             maxParallelThreads = mPT_;
-            primaryServer = primary_;
-            port = port_;
+          
+            ComponentConfig cc = ComponentConfig.GetConfigFromAppConfig();
+            PrimaryServerAddress = cc.ServerAddress;
+            ServerPort = cc.ServerPort;            
         }
 
         public void DivideProblem()
@@ -35,15 +40,7 @@ namespace TaskManager
         }
         public void SendRegisterMessageToServer()
         {
-            List<string> problemNames = new List<string>();
 
-            foreach(TaskSolver ts in ts_List)
-            {
-                problemNames.Add(ts.Name);
-            }
-
-            RegisterMessage message = new RegisterMessage(problemNames, 1, maxParallelThreads, false);
-            message.SerializeAndSend(primaryServer, port);
         }
 
         public void SendMessageToServer()
---
+++ src/TaskManagerTests/TaskManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TaskManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCCTaskSolver;
+
+namespace TaskManagerTests
+{
+    [TestClass]
+    public class TaskManagerTest
+    {
+        [TestMethod]
+        public void TaskManagerInitialize()
+        {
+            TaskManager.TaskManager tm = new TaskManager.TaskManager(40);
+            string server = tm.PrimaryServerAddress;
+            string port = tm.ServerPort;
+
+            //byte[] data = new byte[10];
+            //TaskSolver ts;
+            //ts = new TaskSolver(data);
+            Assert.AreEqual(port, "8080");
+        }
+    }
+}
