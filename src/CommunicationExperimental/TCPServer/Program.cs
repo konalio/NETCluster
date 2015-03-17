b7662eae174d36a1799845e88e709094d4a24a98
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-03-17 15:12:41 +0100

    Working xml message sending

---
+++ src/CommunicationExperimental/TCPServer/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using RegisterMessages;
+
+// State object for reading client data asynchronously
+public class StateObject
+{
+    // Client  socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 1024;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public List<byte> byteBuffer = new List<byte>();
+}
+
+public class AsynchronousSocketListener
+{
+    // Thread signal.
+    public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+    public static void StartListening()
+    {
+        // Data buffer for incoming data.
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.
+        // The DNS name of the computer
+        // running the listener is "host.contoso.com".
+        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.
+        Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+
+            while (true)
+            {
+                // Set the event to nonsignaled state.
+                allDone.Reset();
+
+                // Start an asynchronous socket to listen for connections.
+                Console.WriteLine("Waiting for a connection...");
+                listener.BeginAccept(
+                    new AsyncCallback(AcceptCallback),
+                    listener);
+
+                // Wait until a connection is made before continuing.
+                allDone.WaitOne();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static void AcceptCallback(IAsyncResult ar)
+    {
+        // Signal the main thread to continue.
+        allDone.Set();
+
+        // Get the socket that handles the client request.
+        Socket listener = (Socket)ar.AsyncState;
+        Socket handler = listener.EndAccept(ar);
+
+        // Create the state object.
+        StateObject state = new StateObject();
+        state.workSocket = handler;
+        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            new AsyncCallback(ReadCallback), state);
+    }
+
+    public static void ReadCallback(IAsyncResult ar)
+    {
+        Register content;
+
+        // Retrieve the state object and the handler socket
+        // from the asynchronous state object.
+        StateObject state = (StateObject)ar.AsyncState;
+        Socket handler = state.workSocket;
+
+        // Read data from the client socket. 
+        int bytesRead = handler.EndReceive(ar);
+
+        if (bytesRead > 0)
+        {
+            //TODO
+            //testowy register message tylko z wypełnionym ID udaje wysłać się w jednym rzucie (200 bajtów)
+            //ale trzeba dodać sposób na sprawdzenie czy to całość dostaliśmy żeby móc serializować
+            state.byteBuffer.AddRange(state.buffer);
+
+            //odebrany tu message ma poprawne id
+            // tego nie powinno tu być {
+            var message = Serializers.ByteArrayObject<Register>(state.byteBuffer.ToArray());
+
+            Console.WriteLine("Read {0} bytes from socket. \n",
+                state.byteBuffer.Count);
+
+            var response = new RegisterResponse
+            {
+                Id = message.Id
+            };
+
+            Send(handler, Serializers.ObjectToByteArray(response));
+            // } 
+
+            //handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+              //  new AsyncCallback(ReadCallback), state);
+        }
+        else
+        {
+            Console.WriteLine("Read {0} bytes from socket. \n",
+                state.byteBuffer.Count);
+            content = Serializers.ByteArrayObject<Register>(state.byteBuffer.ToArray());
+            
+            var response = new RegisterResponse
+            {
+                Id = content.Id
+            };
+
+            Send(handler, Serializers.ObjectToByteArray(response));
+        }
+    }
+
+    private static void Send(Socket handler, byte[] byteData)
+    {
+        // Begin sending the data to the remote device.
+        handler.BeginSend(byteData, 0, byteData.Length, 0,
+            new AsyncCallback(SendCallback), handler);
+    }
+
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket handler = (Socket)ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            int bytesSent = handler.EndSend(ar);
+            Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
