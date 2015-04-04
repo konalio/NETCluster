2dc3af111dca0cce890bb41e637c2c322cee14b8
pkupidura <kupidurap@student.mini.pw.edu.pl>
2015-04-04 17:58:11 +0200

    Updated client to master

--- src/ComputationalClient/ComputationalClient.cs
+++ src/ComputationalClient/ComputationalClient.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Xml;
 using ClusterUtils;
-using ClusterUtils.Communication;
 
 namespace ComputationalClient
 {
-    class ComputationalClient
+    class ComputationalClient : Component
     {
-        private readonly ServerInfo _serverInfo;
-
-        public ComputationalClient(ComponentConfig componentConfig)
-        {
-            _serverInfo = new ServerInfo(componentConfig.ServerPort, componentConfig.ServerAddress);
-        }
+        public ComputationalClient(ComponentConfig componentConfig) : base(componentConfig, "ComputationalClient") {}
 
         public void Start()
         {
-            LogClientInfo();
-
+            LogRuntimeInfo();
             var problemId = RequestForSolvingProblem();
-
             WaitForSolution(problemId);
         }
 
@@ -49,41 +41,18 @@ namespace ComputationalClient
                 Id = problemId
             };
 
-            var tcpClient = new ConnectionClient(_serverInfo);
-
-            tcpClient.Connect();
-
-            var response = tcpClient.SendAndWaitForResponses(request)[0];
-
-            tcpClient.Close();
-
-            return response;
+            return SendMessageSingleResponse(request);
         }
 
         private ulong RequestForSolvingProblem()
         {
             var request = new SolveRequest
             {
-                Data = new byte[0],
-                ProblemType = "DVRP"
+                ProblemType = "DVRP",
+                Data = new byte[0]
             };
-
-            var tcpClient = new ConnectionClient(_serverInfo);
-
-            tcpClient.Connect();
-
-            var response = tcpClient.SendAndWaitForResponses(request)[0];
-
-            tcpClient.Close();
-
+            var response = SendMessageSingleResponse(request);
             return ulong.Parse(response.GetElementsByTagName("Id")[0].InnerText);
-        }
-        
-        private void LogClientInfo()
-        {
-            Console.WriteLine("Client is running...");
-            Console.WriteLine("Server address: {0}", _serverInfo.Address);
-            Console.WriteLine("Server port: {0}", _serverInfo.Port);
         }
     }
 }
