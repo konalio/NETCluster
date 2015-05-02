aa4e3b283e384814645c99db683f35d2bc6f1a35
Maja <lechm@student.mini.pw.edu.pl>
2015-05-03 00:27:59 +0200

    TaskSolver & TaskSolverTests
    Both initialized

---
+++ src/TaskSolver/TaskSolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskSolver
+{
+    public class TaskSolver : UCCTaskSolver.TaskSolver
+    {
+        public TaskSolver() : base(new byte[0]) { }
+
+        public override byte[][] DivideProblem(int threadCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] MergeSolution(byte[][] solutions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override byte[] Solve(byte[] partialData, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
