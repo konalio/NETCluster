958304ece9040a18e1af956bebc3bfd45af704ef
Maja <lechm@student.mini.pw.edu.pl>
2015-05-04 01:53:21 +0200

    Problem division and testing it
    DivideProblem method in DVRPTaskSolver and several unit tests in
    DVRPTaskSolverTests.

--- src/DVRPTaskSolver/DVRPTaskSolver.cs
+++ src/DVRPTaskSolver/DVRPTaskSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DVRPTaskSolver
 {
@@ -8,7 +9,42 @@ namespace DVRPTaskSolver
 
         public override byte[][] DivideProblem(int threadCount)
         {
-            throw new NotImplementedException();
+            DVRPData dvrpData = DVRPData.GetFromBytes(base._problemData);
+            List<Request> requests = dvrpData.Requests;
+            int requestsCount = dvrpData.RequestsCount;
+            int[] requestsIds = new int[requestsCount];
+            int[][] requestsSubsets = new int[1][];
+            int[] subset = new int[0];
+            byte[][] returnSubsets;
+
+            for (int i = 0; i < requestsCount; i++)
+                requestsIds[i] = requests[i].Id;
+
+            requestsSubsets[0] = subset;
+
+            SplitIntoSubsetsRecursive(requestsIds, ref requestsSubsets, subset, requestsCount, 0);
+
+            returnSubsets = new byte[requestsSubsets.Length][];
+
+            for (int i = 0; i < requestsSubsets.Length; i++)
+                returnSubsets[i] = DVRPLocationsSubset.Serialize(requestsSubsets[i]);
+
+            return returnSubsets;
+        }
+
+        private void SplitIntoSubsetsRecursive(int[] requestsIds, ref int[][] requestsSubsets, int[] lastSubset, int requestsCount, int lastIndex)
+        {
+            int[] subset;
+            for (int i = lastIndex; i < requestsCount; i++)
+            {
+                subset = (int[])lastSubset.Clone();
+                Array.Resize(ref requestsSubsets, requestsSubsets.Length + 1);
+                Array.Resize(ref subset, subset.Length + 1);
+                subset[subset.Length - 1] = requestsIds[i];
+                requestsSubsets[requestsSubsets.Length - 1] = subset;
+
+                SplitIntoSubsetsRecursive(requestsIds, ref requestsSubsets, subset, requestsCount, i + 1);
+            }
         }
 
         public override byte[] MergeSolution(byte[][] solutions)
