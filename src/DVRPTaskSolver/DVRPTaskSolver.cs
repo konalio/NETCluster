f38a894ed9798ec5dbbf14e4d4cdd84c80a9eebb
Maja <lechm@student.mini.pw.edu.pl>
2015-05-05 22:19:56 +0200

    MergeSolutions method and FinalSolution class

--- src/DVRPTaskSolver/DVRPTaskSolver.cs
+++ src/DVRPTaskSolver/DVRPTaskSolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DVRPTaskSolver
 {
@@ -47,7 +48,80 @@ namespace DVRPTaskSolver
 
         public override byte[] MergeSolution(byte[][] solutions)
         {
-            throw new NotImplementedException();
+            DVRPData dvrpData = DVRPData.GetFromBytes(base._problemData);
+            int vehiclesCount = dvrpData.VehicleCount;
+            List<Request> requests = dvrpData.Requests;
+            int requestsCount = dvrpData.RequestsCount;
+            int[] requestsIds = new int[requestsCount];
+            List<DVRPPartialSolution> subset = new List<DVRPPartialSolution>();
+
+            List<DVRPPartialSolution> partialSolutions = new List<DVRPPartialSolution>();
+            List<List<DVRPPartialSolution>> selectedSolutions = new List<List<DVRPPartialSolution>>();
+            int optimalTime = int.MaxValue;
+            int solutionTime = 0;
+            List<DVRPPartialSolution> finalSolutions = new List<DVRPPartialSolution>();
+            List<int[]> finalSolutionVisits = new List<int[]>();
+
+            for (int i = 0; i < requestsCount; i++)
+                requestsIds[i] = requests[i].Id;
+            
+            for (int i = 0; i < solutions.Length; i++)
+                partialSolutions.Add(DVRPPartialSolution.GetFromByteArray(solutions[i]));
+
+            selectSolutionsRecursive(vehiclesCount, requestsIds, partialSolutions, ref selectedSolutions, subset, solutions.Length, 0);
+
+            foreach(var ss in selectedSolutions)
+            {
+                solutionTime=0;
+                foreach (var s in ss)
+                    solutionTime += s.OptimalTime;
+
+                if(solutionTime<optimalTime)
+                {
+                    optimalTime = solutionTime;
+                    finalSolutions = ss;
+                }
+            }
+
+            foreach (var fs in finalSolutions)
+                finalSolutionVisits.Add(fs.Visits);
+
+            return FinalSolution.Serialize(optimalTime, finalSolutionVisits.ToArray());
+        }
+
+        private void selectSolutionsRecursive(int length, int[] requestsIds, List<DVRPPartialSolution> partialSolutions, ref List<List<DVRPPartialSolution>> selectedSolutions,
+            List<DVRPPartialSolution> lastSubset, int solutionsCount, int lastIndex)
+        {
+            List<DVRPPartialSolution> subset;
+            bool containsElement = false;
+            for (int i = lastIndex; i < solutionsCount; i++)
+            {
+                subset = lastSubset.ConvertAll(solution => solution);
+                subset.Add(partialSolutions[i]);
+
+                if (subset.Count == length)
+                {
+                    foreach (var rId in requestsIds)
+                    {
+                        containsElement = false;
+                        for (int j = 0; j < length; j++)
+                            if (subset[j].RequestsSubset.Contains(rId))
+                            {
+                                if (containsElement)
+                                    return;
+                                containsElement = true;
+                            }
+                        if (!containsElement)
+                            return;
+                    }
+
+                    selectedSolutions.Add(subset);
+
+                    return;
+                }
+
+                selectSolutionsRecursive(length, requestsIds, partialSolutions, ref selectedSolutions, subset, solutionsCount, i + 1);
+            }
         }
 
         public override string Name
